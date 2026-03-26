31bf48eab1bf3c1631ee92fb575c98a1ed27cae1
3llips3s <164566346+3llips3s@users.noreply.github.com>
2026-03-26 19:43:47 +0000

    Deploying to gh-pages from @ 3llips3s/wordle@47d40f05b7b659883e89d01d31b726add7b09ed4 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "50963237"
+    serviceWorkerVersion: "1211635265"
   }
 });
